41738f8db8a5f89a44272a036bf301e3b8dc1a44
Pavel Maslov <dnfanl@gmail.com>
2025-06-04 08:01:32 +0200

    some more test keymap experements

--- nvim/.config/nvim/lua/plugins/vim-test.lua
+++ nvim/.config/nvim/lua/plugins/vim-test.lua
@@ -6,10 +6,10 @@ return {
 	config = function()
 		--Tests
 		vim.keymap.set("n", "<leader>tt", ":TestNearest<CR>", { desc = "[T]est nearest" })
-		vim.keymap.set("n", "<leader>tT", ":TestFile<CR>", { desc = "[T]est file" })
-		vim.keymap.set("n", "<leader>ta", ":TestSuite<CR>", { desc = "Test suite" })
-		vim.keymap.set("n", "<leader>tl", ":TestLast<CR>", { desc = "Test [l]ast" })
-		vim.keymap.set("n", "<leader>tg", ":TestVisit<CR>", { desc = "Test visit" })
+		vim.keymap.set("n", "<leader>T", ":TestFile<CR>", { desc = "[T]est file" })
+		vim.keymap.set("n", "<leader>ta", ":TestSuite<CR>", { desc = "[T]est suite" })
+		vim.keymap.set("n", "<leader>tl", ":TestLast<CR>", { desc = "[T]est [l]ast" })
+		vim.keymap.set("n", "<leader>tg", ":TestVisit<CR>", { desc = "[T]est visit" })
 		vim.cmd("let test#strategy = 'vimux'")
 	end,
 }
