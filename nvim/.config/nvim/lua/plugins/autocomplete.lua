b335b7c443af4d198b2ca0e598a316354e0f4a9e
Pavel Maslov <dnfanl@gmail.com>
2025-06-11 07:58:25 +0200

    go tests made pretty

--- nvim/.config/nvim/lua/plugins/autocomplete.lua
+++ nvim/.config/nvim/lua/plugins/autocomplete.lua
@@ -85,7 +85,6 @@ return {
 				obsidian_new = { name = "obsidian_new", module = "blink.compat.source" },
 				obsidian_tags = { name = "obsidian_tags", module = "blink.compat.source" },
 			},
-			compat = { "obsidian", "obsidian_new", "obsidian_tags" },
 		},
 
 		snippets = { preset = "luasnip" },
