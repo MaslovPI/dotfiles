7fd787d4cad2de7a7442ec849370bdc72456e0cd
Pavel Maslov <pavel.maslov@sedna.com>
2025-07-26 10:25:09 +0200

    snacks

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
 	config = function()
 		require("nvim-treesitter.configs").setup({
 			auto_install = true,
+			ensure_installed = { "regex" },
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
